0547cbcf4e723302605e703394344f4139c77ace
Nelly <kchen0116@gmail.com>
2025-10-19 23:26:44 +1100

    Add unit selection functionality

--- TurnBased-Strategy-Game/Assets/Scripts/Unit.cs
+++ TurnBased-Strategy-Game/Assets/Scripts/Unit.cs
@@ -4,7 +4,14 @@ public class Unit : MonoBehaviour
 {
     private Vector3 targetPosition;
     private float moveSpeed = 4f;
-    private float stoppingDistance = 1f;
+    private float stoppingDistance = .1f;
+    private float rotationSpeed = 10f;
+    [SerializeField] private Animator unityAnimator;
+
+    private void Awake()
+    {
+        targetPosition = transform.position;
+    }
 
     private void Update()
     {
@@ -12,15 +19,17 @@ public class Unit : MonoBehaviour
         {
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             transform.position += moveDirection * Time.deltaTime * moveSpeed;
+
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotationSpeed);
+            unityAnimator.SetBool("isWalking", true);
         }
-        
-        if (Input.GetKeyDown(KeyCode.T))
+        else
         {
-            Move(new Vector3(4, 0, 4));
+            unityAnimator.SetBool("isWalking", true);
         }
     }
 
-    private void Move(Vector3 targetPosition)
+    public void Move(Vector3 targetPosition)
     {
         this.targetPosition = targetPosition;
     }
---
+++ TurnBased-Strategy-Game/Assets/Scripts/UnitActionSystem.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class UnitActionSystem : MonoBehaviour
+{
+    [SerializeField] private Unit selectedUnit;
+    [SerializeField] LayerMask unitLayerMask;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (TryHandleUnitSelection()) return;
+            selectedUnit.Move(MouseWorld.GetPosition());
+        }
+    }
+
+    private bool TryHandleUnitSelection()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out RaycastHit rayCastHit, float.MaxValue, unitLayerMask))
+        {
+            if(rayCastHit.transform.TryGetComponent<Unit>(out Unit unit))
+            {
+                selectedUnit = unit;
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
