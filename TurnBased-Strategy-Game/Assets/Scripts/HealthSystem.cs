fa8d106507b11f48a419ce4375ec070a7c5a1cf9
Nelly <kchen0116@gmail.com>
2025-11-03 23:07:33 +1100

    Add health bar

--- TurnBased-Strategy-Game/Assets/Scripts/HealthSystem.cs
+++ TurnBased-Strategy-Game/Assets/Scripts/HealthSystem.cs
@@ -6,15 +6,25 @@ public class HealthSystem : MonoBehaviour
 {
     [SerializeField] private int health = 100;
     public event EventHandler OnDead;
+    public event EventHandler OnDamaged;
+
+    private int healthMax;
+
+    private void Awake()
+    {
+        healthMax = health;
+    }
 
     public void Damage(int damageAmount)
     {
         health -= damageAmount;
 
-        if(health < 0)
+        if (health < 0)
         {
             health = 0;
         }
+
+        OnDamaged?.Invoke(this, EventArgs.Empty);
 
         if (health == 0)
         {
@@ -25,5 +35,10 @@ public class HealthSystem : MonoBehaviour
     private void Die()
     {
         OnDead?.Invoke(this, EventArgs.Empty);
+    }
+
+    public float GetHealthNormalized()
+    {
+        return (float)health / healthMax;
     }
 }
--- TurnBased-Strategy-Game/Assets/Scripts/UI/UnitWorldUI.cs
+++ TurnBased-Strategy-Game/Assets/Scripts/UI/UnitWorldUI.cs
@@ -1,16 +1,21 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UnitWorldUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI actionPointText;
     [SerializeField] private Unit unit;
+    [SerializeField] private Image healthBarImage;
+    [SerializeField] private HealthSystem healthSystem;
 
     private void Start()
     {
         Unit.OnAnyActionPointsChanged += Unit_OnAnyActionPointsChanged;
+        healthSystem.OnDamaged += HealthSystem_OnDamaged;
         UpdateActionPointText();
+        UpdateHealthBar();
     }
 
     private void UpdateActionPointText()
@@ -21,5 +26,15 @@ public class UnitWorldUI : MonoBehaviour
     private void Unit_OnAnyActionPointsChanged(object sender, EventArgs args)
     {
         UpdateActionPointText();
+    }
+
+    private void UpdateHealthBar()
+    {
+        healthBarImage.fillAmount = healthSystem.GetHealthNormalized();
+    }
+
+    private void HealthSystem_OnDamaged(object sender, EventArgs args)
+    {
+        UpdateHealthBar();
     }
 }
