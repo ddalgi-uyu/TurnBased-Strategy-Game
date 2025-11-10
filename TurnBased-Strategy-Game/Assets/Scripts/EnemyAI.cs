070b3756c78aac04d347806503ea31b2dbbc7344
Nelly <kchen0116@gmail.com>
2025-11-11 02:13:58 +1100

    Add basic enemy AI logic

--- TurnBased-Strategy-Game/Assets/Scripts/EnemyAI.cs
+++ TurnBased-Strategy-Game/Assets/Scripts/EnemyAI.cs
@@ -3,7 +3,20 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    public enum State
+    {
+        WaitingForEnemyTurn,
+        TakingTurn,
+        Busy,
+    }
+
     private float timer;
+    private State state;
+
+    private void Awake()
+    {
+        state = State.WaitingForEnemyTurn;
+    }
 
     private void Start()
     {
@@ -17,15 +30,60 @@ public class EnemyAI : MonoBehaviour
             return;
         }
 
-        timer -= Time.deltaTime;
-        if (timer <= 0)
+        switch (state)
         {
-            TurnSystem.Instance.NextTurn();
+            case State.WaitingForEnemyTurn:
+                break;
+            case State.TakingTurn:
+                timer -= Time.deltaTime;
+                if (timer <= 0)
+                {
+                    if (TryTakeEnemyAIAction(SetStateTakingTurn))
+                    {
+                        state = State.Busy;
+                    }
+                    else
+                    {
+                        // No more enemy have action point left to take
+                        TurnSystem.Instance.NextTurn();
+                    }
+                }
+                break;
+            case State.Busy:
+                break;
         }
+    }
+
+    private void SetStateTakingTurn()
+    {
+        timer = 0.5f;
+        state = State.TakingTurn;
     }
 
     private void TurnSystem_OnTurnChanged(object sender, EventArgs args)
     {
-        timer = 2f;
+        if (!TurnSystem.Instance.IsPlayerTurn())
+        {
+            state = State.TakingTurn;
+            timer = 2f;
+        }
+    }
+
+    private bool TryTakeEnemyAIAction(Action onEnemyActionComplete)
+    {
+        foreach (Unit enemyUnit in UnitManager.Instance.GetEnemyUnitList())
+        {
+            if (TryTakeEnemyAIAction(enemyUnit, onEnemyActionComplete))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private bool TryTakeEnemyAIAction(Unit unit, Action onEnemyActionComplete)
+    {
+        return false;
     }
 }
