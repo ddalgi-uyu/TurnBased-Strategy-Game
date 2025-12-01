7e5a36429dbe079c79186123830f4b9a2c78e809
Nelly <kchen0116@gmail.com>
2025-12-01 23:02:28 +0800

    Add visual debug for pathfingding logic

--- TurnBased-Strategy-Game/Assets/Scripts/Testing.cs
+++ TurnBased-Strategy-Game/Assets/Scripts/Testing.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Testing : MonoBehaviour
@@ -8,5 +9,22 @@ public class Testing : MonoBehaviour
 
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            GridPosition mouseGridPosition = LevelGrid.Instance.GetGridPosition(MouseWorld.GetPosition());
+            GridPosition startGridPosition = new GridPosition(0, 0);
+
+            List<GridPosition> gridPositionList = PathFinding.Instance.FindPath(startGridPosition, mouseGridPosition);
+
+            for (int i = 0; i < gridPositionList.Count; i++)
+            {
+                Debug.DrawLine(
+                    LevelGrid.Instance.GetWorldPosition(gridPositionList[i]),
+                    LevelGrid.Instance.GetWorldPosition(gridPositionList[i + 1]),
+                    Color.white,
+                    10f
+                );
+            }
+        }
     }
 }
